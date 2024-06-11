00c9c296aeaf40f486788ce9b7d1356e0c0be62f
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-06-11 17:57:02 +0500

    added new logos

--- utils/skill-icons.ts
+++ utils/skill-icons.ts
@@ -16,3 +16,33 @@ export const firstRow = [
     title: "Git/Github",
   },
 ];
+
+export const secondRow = [
+  {
+    src: "/skill-icons/mysql.png",
+    title: "MYSQL",
+  },
+  {
+    src: "/skill-icons/mongodb.png",
+    title: "MongoDB",
+  },
+  {
+    src: "/skill-icons/postgresql.png",
+    title: "PostgreSQL",
+  },
+  {
+    src: "/skill-icons/pinecone.png",
+    title: "Pinecone",
+  },
+];
+
+export const lastRow = [
+  {
+    src: "/skill-icons/codeigniter.png",
+    title: "Codeigniter",
+  },
+  {
+    src: "/skill-icons/nestjs.png",
+    title: "NestJS",
+  },
+];

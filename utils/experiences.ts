1952a14bb96f37977398d1d748952dfc8d819188
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-06-11 17:56:49 +0500

    added expreriences

---
+++ utils/experiences.ts
@@ -0,0 +1,34 @@
+import type { TExperience } from "@/types";
+
+export const experiences: TExperience[] = [
+  {
+    from: "July 2023",
+    to: "Present",
+    role: "Full Stack Developer",
+    company: "IONCON Groups LTD. Dubai UAE",
+    description: `As a Full Stack Engineer, I contributed to building a logistics
+              platform for a UAE-based logistics and construction company. My
+              involvement spanned from requirements analysis to the deployment
+              of the product. Alongside my team, I worked on various development
+              tasks, including Payment Gateways: Implemented payment gateways
+              using Stripe and Stripe Connect Secure User Authentication:
+              Developed secure user authentication systems with email and phone
+              verifications Front-End Development: Created responsive and
+              interactive frontends using ReactJS and TailwindCSS.`,
+  },
+  {
+    from: "September 2023",
+    to: "February 2024",
+    role: "Associate Full Stack Developer",
+    company: "Teknotize. Islamabad, Pakistan",
+    description: `As a Full Stack Engineer, I contributed to building a logistics
+              platform for a UAE-based logistics and construction company. My
+              involvement spanned from requirements analysis to the deployment
+              of the product. Alongside my team, I worked on various development
+              tasks, including Payment Gateways: Implemented payment gateways
+              using Stripe and Stripe Connect Secure User Authentication:
+              Developed secure user authentication systems with email and phone
+              verifications Front-End Development: Created responsive and
+              interactive frontends using ReactJS and TailwindCSS.`,
+  },
+];
